89cac9cbf3c5687505c010b9525a55280739148f
Sourav Mansingh <somansin@cisco.com>
2024-01-07 01:11:45 +0530

    Body and Footer revamp

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,57 +8,20 @@ function filterData(searchText, restaurants) {
 };
 
 const Body = () => {
-    const [allRestaurants, setAllRestaurants] = useState(restaurantList);
-    const [filteredRestaurants, setFilteredRestaurants] = useState(restaurantList);
+    const [restaurants, setRestaurants] = useState(restaurantList);
     const [searchText, setSearchText] = useState("");
-    
-    useEffect(() => {
-        // API Call
-        getRestaurants();
-    }, []);
-
-    async function getRestaurants() {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8991793&lng=77.6669909&collection=80424&tags=layout_CCS_Dosa&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
-        const json = await data.json();
-        console.log(json);
-        // Optional Chaining
-        //setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
-        //setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
-    }
-
-    if (!allRestaurants) return null;    // early return
-
-    if(filteredRestaurants.length === 0) return <h1>No Restaurant match your search !!</h1>
-
-    return (allRestaurants.length === 0 ? <Shimmer/> :(
+    return (
         <>
-        <div className="search-container">
-            <input type="text"
-                className="search-input"
-                placeholder="Search"
-                value={searchText}
-                onChange={(e) => {
-                    setSearchText(e.target.value);
-                }}
-            />
-            <button className="search-btn"
-                onClick={() => {                  
-                    const data = filterData(searchText, filteredRestaurants);
-                    setFilteredRestaurants(data);
-                }}
-            >Search-{searchText}</button>
-        </div>
-        
-        <div className="restaurant-list">
-            {/* You have to write logic for No restaurant found here */}
-            {
-                filteredRestaurants.map(restaurant => {
-                    return <RestaurantCard {...restaurant.info} key={restaurant.info.id}/>
-                })
-            }
-        </div>
+            <div className="search-container">
+                <input typ="text" className="search-input" placeholder="Search a restaurant you want.." value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
+                <button className="search-btn" onClick={()=>{const data = filterData(searchText, restaurants);setRestaurants(data)}}>Search</button>
+            </div>
+            <div className="restaurant-list">
+                {restaurants.map((restaurant)=>{return (<RestaurantCard key={restaurant.info.id}{...restaurant.info}/>)})}
+            </div>
         </>
-    ));
+    )
+
 };
 
 export default Body;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,12 @@
 const Footer = () => {
     return (
-        <h4>Footer</h4>
+        <div className="footer">
+            Created By
+            <i className="fa-solid fa-heart"></i>
+            <a href="" target="_blank">Sourav Snigdha Mansingh</a>
+            <i className="fa-solid fa-copyright"></i>2024
+            <strong>Food <span>Fire</span></strong>
+        </div>
     );
 };    
 
